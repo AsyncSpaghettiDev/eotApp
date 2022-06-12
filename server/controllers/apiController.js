022b8445f66849fe307773ac6b316eb61090cb0b
JonathanMov <jafetmv21@gmail.com>
2022-06-11 17:02:47 -0700

    unused modules deleted

--- server/controllers/apiController.js
+++ server/controllers/apiController.js
@@ -1,46 +1,31 @@
-const fetch = require('node-fetch')
 const controller = {};
 
 controller.getTables = async (_, res) => {
-    const response = await fetch('https://eatontime.azurewebsites.net/api/GetTables');
-    const data = await response.json();
-    const fData = JSON.parse(data);
-    res.json(fData);
+    
+    res.json(null);
 }
 
 controller.getEmployees = async (_, res) => {
-    const response = await fetch('https://eatontime.azurewebsites.net/api/GetEmployees ');
-    const data = await response.json();
-    const fData = JSON.parse(data);
-    res.json(fData);
+    
+    res.json(null);
 }
 
 controller.getTableOrder = async (req, res) => {
     const { tableId } = req.query;
-    const response = await fetch(`https://eatontime.azurewebsites.net/api/GetTableOrder?tableId=${tableId}`);
-    const data = await response.json();
-    const fData = JSON.parse(data);
-    res.json(fData);
+    
+    res.json(null);
 }
 
 controller.getCheck = async (req, res) => {
     const { tableID } = req.params;
-    const response = await fetch(`https://eatontime.azurewebsites.net/api/GetCheck/${tableID}`);
-    const data = await response.json();
-    const fData = JSON.parse(data);
-    res.json(fData);
+    
+    res.json(null);
 }
 
 controller.insertIntoOrder = async (req, res) => {
     console.log(req.query);
-    const { tableID } = req.params;
-    console.log(tableID)
-    const response = await fetch(`https://eatontime.azurewebsites.net/api/InsertIntoOrder/${tableID}?productCode={productCode}&qty={qty}`,{
-        method: 'POST'
-    });
-    const data = await response.json();
-    const fData = JSON.parse(data);
-    res.json(fData);
+    
+    res.json(null);
 }
 
 module.exports = controller;
