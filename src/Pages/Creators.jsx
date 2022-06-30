fb11137e2c37e8b0caabf096d3fcb8987cfbdd3b
JonathanMov <jafetmv21@gmail.com>
2022-06-29 21:14:54 -0700

    development team updated

--- src/Pages/Creators.jsx
+++ src/Pages/Creators.jsx
@@ -16,29 +16,9 @@ const Creators = () => {
             </h2>
             <div className="about__section__members">
                 <div className="development__member">
-                    <img className='development__member-image' src={NotFoundImage} alt="Abigail capaceta profile image" />
-                    <h3  className="development__member-name">Abigail Capaceta</h3>
-                    <p   className="development__member-functions">Programador, diseñador, analista.</p>
-                </div>
-                <div className="development__member">
-                    <img className='development__member-image' src={NotFoundImage} alt="Luis Cruz profile image" />
-                    <h3 className="development__member-name">Luis Cruz</h3>
-                    <p className="development__member-functions">Programador, diseñador, administrador, QA.</p>
-                </div>
-                <div className="development__member">
-                    <img className='development__member-image' src={NotFoundImage} alt="Jonathan Mojica profile image" />
+                    <img className='development__member-image' src={NotFoundImage} alt="Jonathan Mojica" />
                     <h3 className="development__member-name">Jonathan Mojica</h3>
                     <p className="development__member-functions">Programador, diseñador, administrador, QA.</p>
-                </div>
-                <div className="development__member">
-                    <img className='development__member-image' src={NotFoundImage} alt="Said Diaz profile image" />
-                    <h3 className="development__member-name">Said Diaz</h3>
-                    <p className="development__member-functions">Programador, diseñador, administrador, QA.</p>
-                </div>
-                <div className="development__member">
-                    <img className='development__member-image' src={NotFoundImage} alt="Susan Ponce profile image" />
-                    <h3 className="development__member-name">Susan Ponce</h3>
-                    <p className="development__member-functions">Programador, analista, diseñador.</p>
                 </div>
             </div>
         </main>
